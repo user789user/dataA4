ef42ffb15bd0ca3eb1a9f7de7d5089cf5b9ae427
Charles Saunders <110saunderi@gmail.com>
2024-11-20 16:35:28 -0500

    please work i am begging you part 2

--- app.py
+++ app.py
@@ -11,12 +11,9 @@ def home():
 if __name__ == '__main__':
     app.run(debug=True)
 
-<<<<<<< HEAD
 #hi how are you
 
 #hadwhfa
 
 #hawfop
-=======
 #this is Dani
->>>>>>> 636ac1832ae73f5a731a190e6e11b61e1a54deeb
